5554c161ec0253b612a255d133e2a5d3c3953db9
Shin seongsu <tlstjdtn321@naver.com>
2019-12-28 23:26:41 +0900

    리스트 까지

--- chapter2.py
+++ chapter2.py
@@ -96,3 +96,205 @@ multiline = """
 """
 print(multiline)
 
+
+# 문자열 더해서 연결하기
+
+head = "Python"
+tail = " is fun!"
+print(head + tail)
+
+# 문자열 곱하기
+a = "python"
+print ( a * 2 )
+
+# 문자열 곱하기 응용
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+
+# 문자열 길이 구하기
+a = "Life is too short"
+print(len(a))
+
+
+# 문자열 인덱싱
+a = "Life is too short, You need Python"
+print(a[3])
+
+# 문자열 슬라이딩
+
+print(a[-2])
+print(a[-5])
+
+print (a[0:4])
+print(a[19:])
+
+print(a[:17])
+print(a[:])
+print(a[19:-7])
+
+# 슬라이싱으로 문자열 나누기
+
+a = "20010331Rainy"
+date = a[:8]
+weather = a[8:]
+
+print(date)
+print(weather)
+
+year = a[:4]
+day = a[4:8]
+weather = a[8:]
+
+print(year)
+print(day)
+print(weather)
+
+a = "Pithon"
+print(a[:1])
+print(a[2:])
+print(a[:1] + 'y' + a[2:])
+
+
+# 문자열 포맷팅
+
+print("I eat %d apples." %3)
+
+print("I eat %s apples. " %"five")
+
+number = 3
+print("I eat %d apples" %number)
+
+
+# 2개 이상의 값 넣기
+
+number = 10
+day = "three"
+print("I ate %d apples. so I was sick for %s days." %(number, day))
+
+print("I have %s apples" % 3)
+print("rate is %s" % 3.234)
+
+print("%10s" % "hi")
+
+print("%-10sjane" % 'hi')
+
+# 소수점 표현하기
+
+print("%0.4f" % 3.42134234)
+print("%10.4f" % 3.42134234)
+
+# format 함수를 사용한 포맷팅
+
+print("I eat {0} apples".format(3))
+
+print("I eat {0} apples".format("five"))
+
+number = 3
+print("I eat {0} apples".format(number))
+
+number = 10
+day = "three"
+print("I eat {0} apples. so I was sick for {1} days".format(number, day))
+
+
+print("I ate {number} apples. so I was sick for {day} days.".format(number=10, day=3))
+print("I ate {0} apples. so I was sick for {day} days".format(10, day=3))
+
+print("{0:<10}".format("hi"))
+print("{0:>10}".format("hi"))
+print("{0:^10}".format("hi"))
+
+print("{0:=^10}".format("hi"))
+print("{0:!<10}".format("hi"))
+print("{0:0.4f}".format(3.42134234))
+
+print("{{ and }}".format())
+
+# f 문자열 포매팅
+name = '홍길동'
+age =30
+print(f'나의 이름은 {name}입니다. 나이는 {age}입니다.')
+
+age = 39
+print(f'나는 내년이면 {age+1}살이 된다.')
+
+d = {'name' : '홍길동', 'age': 30}
+print(f'나의 이름은 {d["name"]}입니다. 나이는 {d["age"]}입니다.')
+
+print(f'{"hi":<10}')
+print(f'{"hi":>10}')
+print(f'{"hi":^10}')
+
+print(f'{"hi":!<10}')
+print(f'{"hi":=^10}')
+
+y = 3.42134234
+print(f'{y:0.4f}')
+print(f'{y:10.4f}')
+
+print(f'{{and}}')
+
+a = "hobby"
+print(a.count('b'))
+
+a = "Python is the best choice"
+print(a.find('b'))
+print(a.find('k'))
+
+a = "Life is too short"
+print(a.index('t'))
+
+print(",".join('abcd'))
+print(",".join(['a','b','c','d']))
+
+a = "hi"
+print(a.upper())
+
+a = " hi "
+print(a.lstrip())
+print(a.rstrip())
+
+a = " hi "
+print(a.strip())
+
+a = "Life is too short"
+print(a.replace("Life", "Your leg"))
+
+a = "Life is too short"
+print(a.split())
+b = "a:b:c:d"
+print(b.split(":"))
+
+
+# 리스트 자료형
+
+a = [1,2,3]
+print(a)
+print(a[0])
+print(a[0] + a[2])
+print(a[-1])
+
+a = [1, 2, 3, ['a', 'b', 'c']]
+print(a[-1])
+print(a[3])
+print(a[-1][0])
+
+# 리스트의 슬라이싱
+
+a = [1, 2, 3, 4, 5]
+print(a[0:2])
+
+a = "12345"
+print(a[0:2])
+
+a = [1, 2, 3, 4, 5]
+b = a[:2]
+c = a[2:]
+
+print(b)
+print(c)
+
+
+
+
