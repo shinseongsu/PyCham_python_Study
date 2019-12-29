fa479418cbc76ff5c5772097e345832083dd9502
Shin seongsu <tlstjdtn321@naver.com>
2019-12-29 22:06:29 +0900

    리스트 마무리

--- chapter2.py
+++ chapter2.py
@@ -296,5 +296,102 @@ print(b)
 print(c)
 
 
+# 리스트 연산하기
+
+a = [1,2,3]
+b = [4,5,6]
+
+print(a + b)
+
+# 리스트 반복하기
+a = [1, 2, 3]
+print(a * 3)
+
+# 리스트 길이 구하기
+print(len(a))
 
 
+# 리스트에서 값 수정하기
+
+a = [1,2,3]
+a[2] = 4
+print(a)
+
+# del 함수로 리스트 요소 삭제하기
+a = [1, 2,3]
+del a[1]
+print(a)
+
+
+a = [1, 2, 3, 4, 5]
+del a[2:]
+print(a)
+
+
+# 리스트에 요소 추가
+
+a = [1,2,3]
+a.append(4)
+print(a)
+
+a.append(([5,6]))
+print(a)
+
+a = [1,4,3,2]
+a.sort()
+print(a)
+
+a = ['a', 'c', 'b']
+a.sort()
+print(a)
+
+
+# 리스트 뒤집기
+
+a = ['a', 'c', 'b']
+a.reverse()
+print(a)
+
+# 위치 반환
+
+a = [1, 2, 3]
+print(a.index(3))
+print(a.index(1))
+
+
+# 리스트에 요소 삽입
+
+a = [1,2,3]
+a.insert(0, 4)
+print(a)
+
+a.insert(3,5)
+print(a)
+
+# 리스트 제거(remove)
+
+a = [1, 2, 3, 1, 2, 3]
+a.remove(3)
+print(a)
+
+#리스트 요소 끄집어내기
+
+a = [1, 2, 3]
+print(a.pop())
+print(a)
+
+a = [1,2,3]
+print(a.pop(1))
+
+# 리스트에 포함된 요소 x의 개수 세기(count)
+
+a = [ 1, 2, 3, 1]
+print(a.count(1))
+
+a  = [1,2,3]
+a.extend([4,5])
+print(a)
+
+b = [6,7]
+a.extend(b)
+print(a)
