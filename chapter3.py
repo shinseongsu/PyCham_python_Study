099244a2c1c703847e4d46a324a318b94cda06b6
Shin seongsu <tlstjdtn321@naver.com>
2019-12-31 21:36:22 +0900

    제어문

---
+++ chapter3.py
@@ -0,0 +1,33 @@
+# if 문 구조
+
+money = True
+
+if money:
+    print("택시를")
+    print("타고")
+    print("가라")
+
+x = 3
+y = 2
+print(x > y)
+
+
+money = 3000;
+if money >= 3000:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+
+
+money = 2000
+card = True
+if money >= 3000 or card:
+    print("택시를 타고가라")
+else:
+    print("걸어가라")
+
+print(1 in [1, 2, 3])
+print(1 not in [1, 2, 3])
+
+print('a' in ['a', 'b', 'c'])
+print('j' in 'python')
